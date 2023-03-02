66052f0a1be4307af57e350c69131f6f04256ade
Chitrakshi Chhabra <73769058+ChitrakshiChhabra@users.noreply.github.com>
2023-03-02 12:14:39 +0530

    Day 2 Problem 2

---
+++ Pascal's Triangle/PrintingAllRows.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<vector<int>> generate(int numRows) {
+
+    vector<vector<int>> ans(numRows);
+    for(int i = 0; i < numRows; i++)
+    {
+        ans.at(i).resize(i + 1);
+        ans.at(i).at(0) = ans.at(i).at(i) = 1;
+        for(int j = 1; j < i; j++)
+            ans.at(i).at(j) = ans.at(i - 1).at(j - 1) + ans.at(i - 1).at(j);
+    }        
+    return ans;
+}
+
+      
+int main()
+{
+    vector<vector<int>> r = generate(5);
+    for(auto a: r)
+    {
+        for(auto b: a)
+            cout << b << " ";
+        cout << endl;
+    }
+  return 0;
+}
