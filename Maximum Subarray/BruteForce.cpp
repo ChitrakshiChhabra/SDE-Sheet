d68c2d2381a8ec19431fa57243d7ab642ede3ca5
Chitrakshi Chhabra <73769058+ChitrakshiChhabra@users.noreply.github.com>
2023-03-02 14:21:21 +0530

    Day 2 Problem 4

---
+++ Maximum Subarray/BruteForce.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int fun(vector<int> &nums)
+{
+    int maxi = 0;
+    for(int i = 0; i < nums.size(); i++)
+    {
+        for(int j = 0; j < nums.size(); j++)
+        {
+            int sum = 0;
+            for(int k = i; k <= j; k++)
+                sum+=nums.at(k);
+            maxi = max(sum, maxi);
+        }
+    }
+    
+    return maxi;
+}
+
+int main()
+{
+    vector <int> nums = {5,4,-1,7,8};
+    cout << fun(nums);
+    return 0;
+}
